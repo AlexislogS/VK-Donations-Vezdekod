0aac2583c6959efd59c536d05d06b6e7e1c08d21
Alex Yatsenko <>
2020-11-30 21:11:10 +0300

    Added rating.

--- VK Donations (Vezdekod)/Controllers/DetailDonationViewController.swift
+++ VK Donations (Vezdekod)/Controllers/DetailDonationViewController.swift
@@ -10,12 +10,16 @@ import UIKit
 
 final class DetailDonationViewController: UIViewController {
     
+    @IBOutlet private weak var ratingLabel: UILabel!
     @IBOutlet private weak var donationProgressView: UIProgressView!
     @IBOutlet private weak var donationImageView: UIImageView!
     @IBOutlet private weak var donationTextView: UITextView!
     @IBOutlet private weak var donationTitle: UILabel!
     @IBOutlet private weak var amountLabel: UILabel!
     @IBOutlet private weak var targetLabel: UILabel!
+  
+    private let trackLayer = CAShapeLayer()
+    private let ratingLayer = CAShapeLayer()
     
     var donation: Donation?
 
@@ -33,6 +37,13 @@ final class DetailDonationViewController: UIViewController {
             }
             animator.startAnimation()
         }
+      let animation = CABasicAnimation(keyPath: "strokeEnd")
+      animation.toValue = 1
+      animation.duration = 2
+      animation.timingFunction = CAMediaTimingFunction(name: .easeIn)
+      animation.fillMode = .backwards
+      animation.isRemovedOnCompletion = false
+      ratingLayer.add(animation, forKey: "rating")
     }
     
     private func updateUI() {
@@ -43,5 +54,38 @@ final class DetailDonationViewController: UIViewController {
         if let imageData = donation?.image {
             donationImageView.image = UIImage(data: imageData)
         }
+        updateRatingLayer()
     }
+  
+  private func updateRatingLayer() {
+    let trackPath = UIBezierPath(
+      roundedRect: CGRect(x: 0,
+                          y: -5,
+                          width: ratingLabel.bounds.width,
+                          height: ratingLabel.bounds.height + 10),
+      cornerRadius: 20
+    )
+    trackLayer.path = trackPath.cgPath
+    trackLayer.lineWidth = trackPath.bounds.height
+    trackLayer.fillColor = UIColor(red: 184/255,
+                                   green: 193/255,
+                                   blue: 204/255,
+                                   alpha: 0.4).cgColor
+    ratingLabel.layer.addSublayer(trackLayer)
+    
+    let ratingPath = UIBezierPath()
+    ratingPath.move(to: CGPoint(x: 0,
+                                y: trackLayer.bounds.minY + 10))
+    ratingPath.addLine(to: CGPoint(x: ratingLabel.bounds.maxX,
+                                   y: trackLayer.bounds.minY + 10))
+    ratingLayer.path = ratingPath.cgPath
+    ratingLayer.lineCap = .round
+    ratingLayer.strokeEnd = 0
+    ratingLayer.strokeColor = UIColor(red: 75/255,
+                                      green: 179/255,
+                                      blue: 75/255,
+                                      alpha: 1).cgColor
+    ratingLayer.lineWidth = trackPath.bounds.height
+    ratingLabel.layer.addSublayer(ratingLayer)
+  }
 }
